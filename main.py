f198a87827ef6b7f1705adac6d36f73a3cd0ba5a
ST_Ryan.Wu 吳勁霆 <ryan.wu@solartninc.com>
2020-07-09 10:56:19 +0800

    20200709

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,make_response
+from flask_cors import cross_origin,CORS
 
 
 
 app = Flask(__name__)
 
+CORS(app, resources=r'/*', supports_credentials=True)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Pn123456@192.168.22.110:3306/17CE"
 db = SQLAlchemy(app)
 
-
-@app.route('/test')
+@app.route('/', methods=['GET'])
+# @cross_origin()
 def index():
 
     sql_cmd = """
@@ -18,8 +21,16 @@ def index():
         """
     query_data = db.engine.execute(sql_cmd)
 
+    result = [dict(row) for row in query_data]
+    response = make_response(jsonify({"result":result}))
 
-    return jsonify({'result': [dict(row) for row in query_data]})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    # rst = make_response(query_data)
+    # rst.headers['Access-Control-Allow-Origin'] = '*'
+
+    return response
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
