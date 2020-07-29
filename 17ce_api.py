a85c53ddd76d632727e1d7e1a13149befb612723
Ryan <jwu34911@gmail.com>
2020-07-29 22:29:24 +0800

    20200729

--- 17ce_api.py
+++ 17ce_api.py
@@ -15,11 +15,287 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://" + DB_user + ":"+ DB_passwd +"@"+ DB_host +":"+ DB_port +"/"+ DB_name
 db = SQLAlchemy(app)
 
-@app.route('/Telecom', methods=['GET'])
-def main_information():
+# 电信
+@app.route('/电信', methods=['GET'])
+def main_information_1():
 
     sql_cmd = """
-        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '电信' AND DATE > date_sub(now(), INTERVAL 3 HOUR)
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '电信'
+        """
+    query_data = db.engine.execute(sql_cmd)
+    date = query_data.fetchall()
+    Date = []
+    Line = []
+    Fastest_node = []
+    Fastest_node_seconds = []
+    Slowest_node = []
+    Slowest_node_seconds = []
+    Average_response = []
+
+    for i in range(len(date)):
+        for index,content in enumerate(date[i]):
+            if index == 0:
+                URL = content
+            elif index == 1:
+                Date.append(content)
+            elif index == 2:
+                Line.append(content)
+            elif index == 3:
+                Fastest_node.append(content)
+            elif index == 4:
+                Fastest_node_seconds.append(content)
+            elif index == 5:
+                Slowest_node.append(content)
+            elif index == 6:
+                Slowest_node_seconds.append(content)
+            elif index == 7:
+                Average_response.append(content)
+
+    response = {
+        "URL":URL,
+        "Line":Line,
+        "Date":Date,
+        "Fastest_node":Fastest_node,
+        "Fastest_node_seconds":Fastest_node_seconds,
+        "Slowest_node":Slowest_node,
+        "Slowest_node_seconds":Slowest_node_seconds,
+        "Average_response":Average_response
+    }
+    response = make_response(jsonify(response))
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+
+    return response
+
+#-----------------------------------------------------------------------
+# 联通
+@app.route('/联通', methods=['GET'])
+def main_information_2():
+
+    sql_cmd = """
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '联通'
+        """
+    query_data = db.engine.execute(sql_cmd)
+    date = query_data.fetchall()
+    Date = []
+    Line = []
+    Fastest_node = []
+    Fastest_node_seconds = []
+    Slowest_node = []
+    Slowest_node_seconds = []
+    Average_response = []
+
+    for i in range(len(date)):
+        for index,content in enumerate(date[i]):
+            if index == 0:
+                URL = content
+            elif index == 1:
+                Date.append(content)
+            elif index == 2:
+                Line.append(content)
+            elif index == 3:
+                Fastest_node.append(content)
+            elif index == 4:
+                Fastest_node_seconds.append(content)
+            elif index == 5:
+                Slowest_node.append(content)
+            elif index == 6:
+                Slowest_node_seconds.append(content)
+            elif index == 7:
+                Average_response.append(content)
+
+    response = {
+        "URL":URL,
+        "Line":Line,
+        "Date":Date,
+        "Fastest_node":Fastest_node,
+        "Fastest_node_seconds":Fastest_node_seconds,
+        "Slowest_node":Slowest_node,
+        "Slowest_node_seconds":Slowest_node_seconds,
+        "Average_response":Average_response
+    }
+    response = make_response(jsonify(response))
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+
+    return response
+
+#-----------------------------------------------------------------------
+# 移动
+@app.route('/移动', methods=['GET'])
+def main_information_3():
+
+    sql_cmd = """
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '移动'
+        """
+    query_data = db.engine.execute(sql_cmd)
+    date = query_data.fetchall()
+    Date = []
+    Line = []
+    Fastest_node = []
+    Fastest_node_seconds = []
+    Slowest_node = []
+    Slowest_node_seconds = []
+    Average_response = []
+
+    for i in range(len(date)):
+        for index,content in enumerate(date[i]):
+            if index == 0:
+                URL = content
+            elif index == 1:
+                Date.append(content)
+            elif index == 2:
+                Line.append(content)
+            elif index == 3:
+                Fastest_node.append(content)
+            elif index == 4:
+                Fastest_node_seconds.append(content)
+            elif index == 5:
+                Slowest_node.append(content)
+            elif index == 6:
+                Slowest_node_seconds.append(content)
+            elif index == 7:
+                Average_response.append(content)
+
+    response = {
+        "URL":URL,
+        "Line":Line,
+        "Date":Date,
+        "Fastest_node":Fastest_node,
+        "Fastest_node_seconds":Fastest_node_seconds,
+        "Slowest_node":Slowest_node,
+        "Slowest_node_seconds":Slowest_node_seconds,
+        "Average_response":Average_response
+    }
+    response = make_response(jsonify(response))
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+
+    return response
+
+#-----------------------------------------------------------------------
+# 海外
+@app.route('/海外', methods=['GET'])
+def main_information_4():
+
+    sql_cmd = """
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '海外'
+        """
+    query_data = db.engine.execute(sql_cmd)
+    date = query_data.fetchall()
+    Date = []
+    Line = []
+    Fastest_node = []
+    Fastest_node_seconds = []
+    Slowest_node = []
+    Slowest_node_seconds = []
+    Average_response = []
+
+    for i in range(len(date)):
+        for index,content in enumerate(date[i]):
+            if index == 0:
+                URL = content
+            elif index == 1:
+                Date.append(content)
+            elif index == 2:
+                Line.append(content)
+            elif index == 3:
+                Fastest_node.append(content)
+            elif index == 4:
+                Fastest_node_seconds.append(content)
+            elif index == 5:
+                Slowest_node.append(content)
+            elif index == 6:
+                Slowest_node_seconds.append(content)
+            elif index == 7:
+                Average_response.append(content)
+
+    response = {
+        "URL":URL,
+        "Line":Line,
+        "Date":Date,
+        "Fastest_node":Fastest_node,
+        "Fastest_node_seconds":Fastest_node_seconds,
+        "Slowest_node":Slowest_node,
+        "Slowest_node_seconds":Slowest_node_seconds,
+        "Average_response":Average_response
+    }
+    response = make_response(jsonify(response))
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+
+    return response
+
+#-----------------------------------------------------------------------
+# 教育网
+@app.route('/教育网', methods=['GET'])
+def main_information_5():
+
+    sql_cmd = """
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '教育网'
+        """
+    query_data = db.engine.execute(sql_cmd)
+    date = query_data.fetchall()
+    Date = []
+    Line = []
+    Fastest_node = []
+    Fastest_node_seconds = []
+    Slowest_node = []
+    Slowest_node_seconds = []
+    Average_response = []
+
+    for i in range(len(date)):
+        for index,content in enumerate(date[i]):
+            if index == 0:
+                URL = content
+            elif index == 1:
+                Date.append(content)
+            elif index == 2:
+                Line.append(content)
+            elif index == 3:
+                Fastest_node.append(content)
+            elif index == 4:
+                Fastest_node_seconds.append(content)
+            elif index == 5:
+                Slowest_node.append(content)
+            elif index == 6:
+                Slowest_node_seconds.append(content)
+            elif index == 7:
+                Average_response.append(content)
+
+    response = {
+        "URL":URL,
+        "Line":Line,
+        "Date":Date,
+        "Fastest_node":Fastest_node,
+        "Fastest_node_seconds":Fastest_node_seconds,
+        "Slowest_node":Slowest_node,
+        "Slowest_node_seconds":Slowest_node_seconds,
+        "Average_response":Average_response
+    }
+    response = make_response(jsonify(response))
+
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+
+    return response
+
+#-----------------------------------------------------------------------
+# 台湾
+@app.route('/台湾', methods=['GET'])
+def main_information_6():
+
+    sql_cmd = """
+        SELECT * from main_information WHERE URL = 'smtv.io' AND Line = '台湾'
         """
     query_data = db.engine.execute(sql_cmd)
     date = query_data.fetchall()
@@ -69,25 +345,5 @@ def main_information():
     return response
 
 
-
-@app.route('/node_information', methods=['GET'])
-def node_information():
-
-    sql_cmd = """
-        select * from node_information WHERE DATE > date_sub(now(), INTERVAL 1 hour)
-        """
-    query_data = db.engine.execute(sql_cmd)
-
-    result = [dict(row) for row in query_data]
-    response = make_response(jsonify({"result":result}))
-
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = '*'
-
-
-    return response
-
 if __name__ == "__main__":
-    app.run( host='192.168.0.12',port=5050, debug=True)
-
+    app.run( host='192.168.0.10',port=5000, debug=True)
